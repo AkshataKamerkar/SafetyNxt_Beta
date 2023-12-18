c56bee2a466ab8850c804653bc7e02033287d12f
AK <akshatakamerkar4@gmail.com>
2023-12-18 18:56:28 +0530

    Regular Update 0

--- Main/views.py
+++ Main/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 
+'''
+    Total 5 Routes 
+        - LandingPage      : Nav, MainVideo/ MainModel, Services, Features, Contact Form, Testimonials, Footer 
+        - About Page       : Nav, About Content, Footer 
+        - LogInSignUp Page : Nav, LogIn/SignUp Form, Footer 
+        - GetLLs Page      : Formed based page to take start location and end location from the user along with the Start Monitoring Button 
+        - Map Page         : Here the results will be displayed
+        
+    Total 3 Forms 
+        - Contact Us : On LandingPage 
+        - Login Form : On LogInSignUp Page 
+        - get_loc    : On GetLLs Page    
+'''
 
 # Creating the Landing Page
-class LandingPage(TemplateView):
+# LandingPage is a TemplateView since we just hv to render a simple template and dont have to perform form based or data retrival frm the models operations
+class LandingPage(FormView):
+
 
     template_name = 'landingPage.html'
 
