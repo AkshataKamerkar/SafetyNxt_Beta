28999213b9ca2873522718feb8f42ce68fb76705
AK <akshatakamerkar4@gmail.com>
2023-12-26 14:02:21 +0530

    Added the Registration and Authentication Logic [ without final UI ]

--- SafetyNxt/urls.py
+++ SafetyNxt/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from Main import urls as main_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(main_urls,namespace='main')),
-]
+    path('accounts/',include('allauth.urls')),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
