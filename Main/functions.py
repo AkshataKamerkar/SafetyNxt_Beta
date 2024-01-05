b1ff9de6daa40664df8b87760c5b58106d125a0e
AK <akshatakamerkar4@gmail.com>
2024-01-06 00:07:42 +0530

    Added Location Manipulation Functions

--- Main/functions.py
+++ Main/functions.py
@@ -1,4 +1,5 @@
 import geocoder
+from geopy import Nominatim
 
 '''
     This file will contain all the functions that are used in backend for user input manipulation as well as to fetch the appropriate cctv id's
