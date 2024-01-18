39849cae26520090ed572669c85301970d4a5709
Akanksha <akankshapalve63@gmail.com>
2024-01-18 12:18:59 +0530

    added csv files

--- Main/urls.py
+++ Main/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('',LandingPage.as_view(),name='index'),
     path('about',AboutUs.as_view(),name='about'),
-    path('info',GetLLs.as_view(),name='info'),
+    path('info/',GetLLs.as_view(),name='info'),
     path('log',LogInSignUp.as_view(),name='log'),
     path('menu',Main.as_view(),name='menu'),
 ]
